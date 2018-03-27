75025c241e8588ec8f8d9c7702fe88c858dc0129
KiproshMacAir10 <kiproshmacair10@KiproshMacAir10s-MacBook-Air.local>
2018-03-27 18:22:55 +0530

    filtered checkbox list added

--- app/controllers/authors/new.js
+++ app/controllers/authors/new.js
@@ -3,10 +3,23 @@ import Controller from '@ember/controller';
 export default Controller.extend({
 	selectedBooks: [],
 
+	isAdmin: true,
+
+	checkboxObjects: [
+		    Ember.Object.create({ id: "1", name: 'First Object',  isAdmin: false}),
+		    Ember.Object.create({ id: "2", name: 'Second',  isAdmin: false}),
+		    Ember.Object.create({ id: "3", name: 'Third',  isAdmin: false}),
+		    Ember.Object.create({ id: "4", name: 'Fourth', isAdmin: false })
+	],
+
 	actions: {
 		selectBook(event) {
-      const selectedBooks = Ember.$(event.target).val();
-      this.set('selectedBooks', selectedBooks || []);
-    }
+	      const selectedBooks = Ember.$(event.target).val();
+	      this.set('selectedBooks', selectedBooks || []);
+	    },
+
+	    getFilteredCheckbox(){
+	    	let result = this.get('checkboxObjects').filterBy('isAdmin');
+	    }
 	}
 });
